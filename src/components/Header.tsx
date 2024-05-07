bc3c6ce521a932c10706380b080acfa51ed18918
nseah21 <nseah21@gmail.com>
2024-05-07 22:53:49 +0800

    Handle data fetching in UI components

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,7 +8,53 @@ type MetadataEntry = {
     id: string
 }
 
-export default function Header({ firstPrize, secondPrize, thirdPrize, drawDate, metadata }: { firstPrize: string, secondPrize: string, thirdPrize: string, drawDate: string, metadata: MetadataEntry[] }) {
+export default function Header() {
+    const [currentDrawId, setCurrentDrawId] = useState("");
+    const [firstPrize, setFirstPrize] = useState("");
+    const [secondPrize, setSecondPrize] = useState("");
+    const [thirdPrize, setThirdPrize] = useState("");
+    const [drawDate, setDrawDate] = useState("")
+    const [metadata, setMetadata] = useState<MetadataEntry[]>([]);
+    const [loading, setLoading] = useState(true);
+
+    const handleLoadNewResults = async (event: React.MouseEvent<HTMLSelectElement>) => {
+        const drawId = event.currentTarget.value;
+        if (drawId == currentDrawId) {
+            return;
+        }
+
+        setLoading(true);
+
+        const newResults = await fetch(`https://teletext-server.vercel.app/draws/${drawId}`);
+        const newResultsJson = await newResults.json();
+
+        setCurrentDrawId(newResultsJson.id);
+        setFirstPrize(newResultsJson.first);
+        setSecondPrize(newResultsJson.second);
+        setThirdPrize(newResultsJson.third);
+        setDrawDate(newResultsJson.date);
+        setLoading(false);
+    }
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const latestDraw = await fetch("https://teletext-server.vercel.app/latest");
+            const metadata = await fetch("https://teletext-server.vercel.app/metadata");
+            const latestDrawJson = await latestDraw.json();
+            const metadataJson = await metadata.json();
+
+            setCurrentDrawId(latestDrawJson.id);
+            setFirstPrize(latestDrawJson.first);
+            setSecondPrize(latestDrawJson.second);
+            setThirdPrize(latestDrawJson.third);
+            setDrawDate(latestDrawJson.date);
+            setMetadata(metadataJson);
+            setLoading(false);
+        };
+
+        fetchData().catch(console.error);
+    }, []);
+
     return (
         <div className="flex p-4">
             <div className="bg-huat-red p-4">
@@ -25,10 +71,10 @@ export default function Header({ firstPrize, secondPrize, thirdPrize, drawDate, 
                         </ul>
                         <ul>
                             Display results for:
-                            <select className="bg-jagung-yellow pl-4 pt-2">
+                            <select onClick={handleLoadNewResults} className="bg-jagung-yellow pl-4 pt-2">
                                 {
                                     metadata.map((entry, id) => {
-                                        return <option key={id} value={entry.date}></option>
+                                        return <option key={id} value={entry.id}>{entry.date}</option>
                                     })
                                 }
                             </select>
@@ -37,11 +83,11 @@ export default function Header({ firstPrize, secondPrize, thirdPrize, drawDate, 
                     </div>
                 </div>
                 <div className="text-center">
-                    <div className="p-2 pt-3">{drawDate}</div>
+                    <div className="p-2 pt-3">{loading ? "Loading..." : drawDate}</div>
                     <div className="pt-4 bg-black text-jagung-yellow space-x-[100px]">
-                        <span>1st: {firstPrize}</span>
-                        <span>2nd: {secondPrize}</span>
-                        <span>3rd: {thirdPrize}</span>
+                        <span>{!loading && `1st: ${firstPrize}`}</span>
+                        <span>{!loading && `2nd: ${secondPrize}`}</span>
+                        <span>{!loading && `3rd: ${thirdPrize}`}</span>
                     </div>
                 </div>
             </div>
@@ -49,21 +95,3 @@ export default function Header({ firstPrize, secondPrize, thirdPrize, drawDate, 
     )
 }
 
-export async function getServerSideProps() {
-    const latestDraw = await fetch("https://teletext-server.vercel.app/latest");
-    const metadata = await fetch("https://teletext-server.vercel.app/metadata");
-    
-    const latestDrawJson = await latestDraw.json(); 
-    const metadataJson = await metadata.json(); 
-
-    return {
-        props: {
-            firstPrize: latestDrawJson.first,
-            secondPrize: latestDrawJson.second,
-            thirdPrize: latestDrawJson.third,
-            drawDate: latestDrawJson.date,
-            metadata: metadataJson
-        }
-    }
-
-}
